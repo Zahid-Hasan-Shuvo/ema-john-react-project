7724ef35f885a70790de8224042cd17b45160806
Zahid-Hasan-Shuvo <shuvo.cse6.bu@gmail.com>
2023-04-12 03:03:50 +0600

    deploy fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './component/Header/Header'
-import Shop from './component/Shop/Shop'
+import { useState } from "react";
+
+import "./App.css";
+import Header from "./component/Header/Header";
+import Shop from "./component/Shop/Shop";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-   <Header></Header>
-   <Shop></Shop>
- 
+      <Header></Header>
+      <Shop></Shop>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/component/Product/Product.jsx
+++ src/component/Product/Product.jsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import './Product.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-  import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import "./Product.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 const Product = (props) => {
-  const {img, name, seller, ratings, price, } =props.product;
+  const { img, name, seller, ratings, price } = props.product;
 
-  const handleAddToCart= props.handleAddToCart;
+  const handleAddToCart = props.handleAddToCart;
   return (
-    <div className='product'>
+    <div className="product">
       <img src={img} alt="" />
-     <div className='product-info'>
-     <h6 className='product-name'>{name}</h6>
-      <p>Price: ${price}</p>
-      <p>Manufacturer: {seller}</p>
-      <p>Rating: {ratings}</p>
-     </div>
-     <button onClick={()=>handleAddToCart()} className='btn-cart'>
-      
-      Add to cart
-      <FontAwesomeIcon icon={faShoppingCart} />
+      <div className="product-info">
+        <h6 className="product-name">{name}</h6>
+        <p>Price: ${price}</p>
+        <p>Manufacturer: {seller}</p>
+        <p>Rating: {ratings}</p>
+      </div>
+      <button onClick={() => handleAddToCart()} className="btn-cart">
+        Add to cart
+        <FontAwesomeIcon icon={faShoppingCart} />
       </button>
     </div>
   );
